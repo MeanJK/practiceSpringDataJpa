8c562186a7a603380aea995cb1c1a67c6ee555d2
MJ <rlaalswls102@naver.com>
2024-04-15 17:24:34 +0900

    4/15 - 스프링 데이터 JPA 페이징과 Slice

--- src/main/java/jpaStudy/datajpa/repository/MemberRepository.java
+++ src/main/java/jpaStudy/datajpa/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package jpaStudy.datajpa.repository;
 
 import jpaStudy.datajpa.dto.MemberDto;
 import jpaStudy.datajpa.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,4 +34,5 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findMemberByUsername(String username); //단건
     Optional<Member> findOptionalMemberByUsername(String username); //단건 Optional
 
+    Page<Member> findByAge(int age, Pageable pageable);
 }
--- src/test/java/jpaStudy/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/jpaStudy/datajpa/repository/MemberRepositoryTest.java
@@ -3,10 +3,12 @@ package jpaStudy.datajpa.repository;
 import jpaStudy.datajpa.dto.MemberDto;
 import jpaStudy.datajpa.entity.Member;
 import jpaStudy.datajpa.entity.Team;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +17,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -118,5 +119,34 @@ class MemberRepositoryTest {
 
         Optional<Member> findMember2 = memberRepository.findOptionalMemberByUsername("asdfasdfasdf");
         System.out.println("findMember2 = " + findMember2); //Optional.empty반환
+    }
+
+    @Test
+    public void testPaging(){
+        //given
+        memberRepository.save(new Member("aaa", 10));
+        memberRepository.save(new Member("bbb", 10));
+        memberRepository.save(new Member("ccc", 10));
+        memberRepository.save(new Member("ddd", 10));
+        memberRepository.save(new Member("eee", 10));
+        //when
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+
+        Page<Member> pages = memberRepository.findByAge(age, pageRequest);
+
+        //Member 엔티티 상태로 반환하면 좋지 않다고 전부터 누누히 말했다. Dto로 변환시켜 반환하기!
+        Page<MemberDto> toMap = pages.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+
+        List<Member> content = pages.getContent();
+        long totalCount = pages.getTotalElements();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(pages.getTotalElements()).isEqualTo(5);
+        assertThat(pages.getNumber()).isEqualTo(0);
+        assertThat(pages.getTotalPages()).isEqualTo(2);
+        assertThat(pages.isFirst()).isTrue();
+        assertThat(pages.hasNext()).isTrue();
     }
 }
