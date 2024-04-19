d517c8a7177c3ba7c32b9b3755dd8b854a50ecdb
MJ <rlaalswls102@naver.com>
2024-04-19 21:23:31 +0900

    4/19 - 페이징과 정렬

--- src/main/java/jpaStudy/datajpa/controller/MemberController.java
+++ src/main/java/jpaStudy/datajpa/controller/MemberController.java
@@ -1,9 +1,12 @@
 package jpaStudy.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
+import jpaStudy.datajpa.dto.MemberDto;
 import jpaStudy.datajpa.entity.Member;
 import jpaStudy.datajpa.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +28,18 @@ public class MemberController {
         return member.getUsername();                                //조회할 때만 사용하기.
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> listMembers(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        //PageRequest request = PageRequest.of(1, 2) 이런식으로 구현체를 직접 만들어서 페이지를 지정할 수 있다.
+        Page<MemberDto> map = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+        return map;
+    }
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("member1"));
+        for(int i = 0; i <= 100; i++){
+            memberRepository.save(new Member("user"+i, i));
+        }
     }
 }
